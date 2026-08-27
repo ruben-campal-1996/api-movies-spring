fc77403e24db52b0a7ef544a90ffc98574f6626c
Ruben <ruben.campal.lopez1996@gmail.com>
2026-08-27 12:55:55 +0200

    feat: add FilmService initial methods

--- src/main/java/ruben/dev/api_movies/service/FilmsService.java
+++ src/main/java/ruben/dev/api_movies/service/FilmsService.java
@@ -1,5 +1,35 @@
 package ruben.dev.api_movies.service;
 
-public class FilmsService {
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+
+
+import ruben.dev.api_movies.entity.FilmsEntity;
+import ruben.dev.api_movies.repository.FilmsRepository;
+
+@Service
+public class FilmsService {
+    private final FilmsRepository filmsRepository;
+
+    public FilmsService(FilmsRepository filmsRepository) {
+        this.filmsRepository = filmsRepository;
+    }
+
+    public List<FilmsEntity> findAll() {
+        return filmsRepository.findAll();
+    }
+
+    public Optional<FilmsEntity> findById(long id){
+        return filmsRepository.findById(id);
+    }
+
+    public FilmsEntity save(FilmsEntity film) {
+        return filmsRepository.save(film);
+    }
+
+    public void deleteById(Long id) {
+        filmsRepository.deleteById(id);
+    }
 }
